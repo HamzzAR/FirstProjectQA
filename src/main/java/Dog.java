ce6a910733265d60988c7296b381746c3b3293f8
Hamza <hamza_razeq@hotmail.co.uk>
2019-04-15 15:14:27 +0100

    Init commit

---
+++ src/main/java/Dog.java
@@ -0,0 +1,18 @@
+
+public class Dog {
+	
+	public String name;
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String bark() {
+		
+		return "Woof";
+	}
+	
+	
+	
+	
+}
